e616bc0d44609c2c90163c18b054397d57dee3ea
dharani1700 <ds6687@srmist.edu.in>
2026-07-31 23:14:27 +0530

    Implemented interactive stock list

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
+const listsection = document.getElementById("list-section");
 const STOCK_API =
   "https://stock-market-api-k9vl.onrender.com/api/stocksstatsdata";
-
+let currentstock="";
 async function fetchStocks() {
+  try {
+    const response = await fetch(STOCK_API);
+    const data = await response.json();
 
-    try {
+    const stockData = data.stocksStatsData[0];
 
-        console.log("Fetching Data...");
+    for (let stock in stockData) {
+      //console.log(stock);
+      //console.log(stockData[stock]);
+      //console.log(stockData[stock].bookValue);
+      //console.log(stockData[stock].profit);
 
-        const response = await fetch(STOCK_API);
+      const card = document.createElement("div");
+      card.classList.add("stock-card");
+      card.addEventListener("click", () => {
+        currentstock=stock;
+        console.log(`clicked on ${stock}`);
+      });
+      const name = document.createElement("h2");
+      name.textContent = stock;
+      const bookValue = document.createElement("p");
+      bookValue.textContent = `Book Value : ${stockData[stock].bookValue}`;
+      const profit = document.createElement("p");
+      profit.textContent = `Profit : ${(stockData[stock].profit * 100).toFixed(2)}%`;
 
-        const data = await response.json();
-
-        console.log(data);
-
+      card.appendChild(name);
+      card.appendChild(bookValue);
+      card.appendChild(profit);
+      listsection.appendChild(card);
     }
-    catch(error){
-
-        console.log(error);
-
-    }
-
+  } catch (error) {
+    console.log(error);
+  }
 }
+
 fetchStocks();
