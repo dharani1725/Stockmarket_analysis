4a43f7d45063dd24247324a9f4a326668d6c06a9
dharani1700 <ds6687@srmist.edu.in>
2026-08-01 16:30:31 +0530

    Implemented dynamic stock chart with Chart.js

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
-const listsection = document.getElementById("list-section");
+//Calling API
+const CHART_API = "https://stock-market-api-k9vl.onrender.com/api/stocksdata";
 const STOCK_API =
   "https://stock-market-api-k9vl.onrender.com/api/stocksstatsdata";
-let currentstock="";
+const DETAILS_API = "https://stock-market-api-k9vl.onrender.com/api/profiledata";
+//Selecting the elements from the DOM
+const listsection = document.getElementById("list-section");
+const chartCanvas = document.querySelector("#chart");
+const btn = document.querySelector("#buttons");
+
+//Initializing variables to store the current required information
+let currentstock = "";
+let currentduration = "3mo";
+let chartInstance = null;
+
+// Adding event listeners to the buttons to load the chart with the selected duration
+for (let button of btn.children) {
+  button.addEventListener("click", () => {
+    if (currentstock === "") {
+      alert("Please select a stock first.");
+      return;
+    }
+    currentduration = button.textContent;
+    loadChart(currentstock, currentduration);
+  });
+}
+
+
+// Function to fetch stock data and create stock cards
 async function fetchStocks() {
   try {
     const response = await fetch(STOCK_API);
     const data = await response.json();
-
     const stockData = data.stocksStatsData[0];
 
     for (let stock in stockData) {
-      //console.log(stock);
-      //console.log(stockData[stock]);
-      //console.log(stockData[stock].bookValue);
-      //console.log(stockData[stock].profit);
-
       const card = document.createElement("div");
       card.classList.add("stock-card");
       card.addEventListener("click", () => {
-        currentstock=stock;
-        console.log(`clicked on ${stock}`);
+        currentstock = stock;
+        loadChart(stock, currentduration);
       });
       const name = document.createElement("h2");
       name.textContent = stock;
@@ -37,5 +56,67 @@ async function fetchStocks() {
     console.log(error);
   }
 }
+// Call the stocks value and timeline data from the API and load the chart
+async function loadChart(stockname, duration) {
+  try {
+    const response = await fetch(CHART_API);
+    const data = await response.json();
 
-fetchStocks();
+    const values = data.stocksData[0][stockname][duration].value;
+    const timestamps = data.stocksData[0][stockname][duration].timeStamp;
+    const labels = [];
+
+    for (let i = 0; i < timestamps.length; i++) {
+      const date = new Date(timestamps[i] * 1000);
+      const formattedDate = date.toLocaleDateString();
+      labels.push(formattedDate);
+    }
+
+    // Destroy the previous chart instance if it exists
+    if (chartInstance) {
+      chartInstance.destroy();
+    }
+    // Create a new chart
+    const ctx = chartCanvas.getContext("2d");
+    chartInstance = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: stockname,
+            data: values,
+            backgroundColor: "rgba(255, 99, 132, 0.2)",
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
+            fill: false,
+            pointRadius: 0,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//fetchStocks();
+
+async function stockdetails(stockname) {
+  try {
+    const response = await fetch(DETAILS_API);
+    const data = await response.json();
+    const stockData = data.stocksProfileData[0][stockname]["summary"];
+    console.log(stockData);
+  } catch (error) {
+    console.log(error);
+  }
+}
+stockdetails("AAPL");
